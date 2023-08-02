d8e00336d35fb63a9642fbabf35c5ddeea9fe921
ZH-Himel <zh.himel.2002@gmail.com>
2023-08-02 16:26:48 +0600

    admin user diffenetiate in log in

---
+++ server/models/order-details.js
@@ -0,0 +1,25 @@
+const mongoose=require('mongoose');
+const newSchema=new mongoose.Schema({
+    email:{
+        type:String,
+        required:true
+    },
+    phone:{
+        type:String,
+        required:true
+    },
+    amount:{
+        type:String,
+        required:true
+    },
+    address:{
+        type:String,
+        required:true
+    },
+    date:{
+        type:String,
+        required:true
+    }
+})
+const orderdetails=mongoose.model("orderdetails",newSchema);
+module.exports=user;
